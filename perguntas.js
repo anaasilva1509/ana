041c55920bae4f9319d9a7b38400b8e7fec04891
anaasilva1509 <laura.silvadamata@gmail.com>
2024-11-25 13:09:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geral',
+    ' qual o lugar que as crianças e adolescentes vão para aprender?',
+    'Escola'
+)
+
+criaCartao(
+    'Geral',
+    'qual o maior planeta do sistema solar?',
+    'Júpiter'
+)
+
+criaCartao(
+    'Geral',
+    'De onde é a invenção do chuveiro elétrico?',
+    'Brasil'
+)
+
+criaCartao(
+    'Geral',
+    'Atualmente, quantos elementos químicos a tabela periódica possui?',
+    '118 '
+)
